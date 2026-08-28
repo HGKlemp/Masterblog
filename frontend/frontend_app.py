faeed54f042a3ddfbd18ea5926e338144391e2c0
Guenter Klemp <g.klemp@gmx.de>
2026-08-28 15:41:54 +0200

    frontend_app refactored

--- frontend/frontend_app.py
+++ frontend/frontend_app.py
@@ -3,10 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def home():
+    """Render the main blog page."""
     return render_template("index.html")
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5001, debug=True)
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=5001,
+        debug=True
+    )
